ee0fe3ce3c3d7ab82defdbc7057b924506be4a29
Gabriel <gabrielturing@gmail.com>
2021-03-22 23:50:54 -0300

    NPC EDITOR

--- server/src/routes.js
+++ server/src/routes.js
@@ -14,32 +14,6 @@ const authenticate = require('./middlewares/authenticate');
 
 routes.get('/', homeController);
 
-<<<<<<< HEAD
-const { getNativeFunction } = require('sbffi');
-
-routes.get('/teste', async (req, res, next) => {
-    try {
-        const authLibrary = getNativeFunction('D:\\_TRABALHO\\wyd-spirit-web\\server\\src\\addons\\StaticLib1.so', 'Subtract', 'int', ['int', 'int']);
-        res.send(authLibrary(15, 15));
-    } catch (err) {
-=======
-const ffi = require('ffi-napi');
-
-routes.get('/teste', async (req, res, next) => {
-    try {
-        const authLibrary = new ffi.Library('./addons/readAccount.o', {
-            'Subctract': {
-                'int': ['int', 'int']
-            }
-        });
-        res.send(authLibrary.Subctract(15, 15));
-    } catch (err) {
-        console.log(process.platform);
->>>>>>> parent of 61608be (Init away)
-        res.send(err.toString());
-    }
-});
-
 routes.use(authenticate);
 
 routes  .route('/users')
